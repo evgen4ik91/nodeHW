3adecfdb66939a74b50706cd99ab3057ed3dec53
yauheni_chyrkouski <evgen.box@live.ru>
2019-01-15 14:18:25 +0300

    routes and logger

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const port = 3000;
 
 const news = require('./news.json');
 
-app.route('/news')
+function getTime() {
+    return new Date().toLocaleString(undefined, {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        day:'numeric',
+        month: 'short',
+        year: 'numeric',
+    });
+}
+
+router.use(function timeLog (req, res, next) {
+    console.log( `URL: ${req.url} (${req.method}) Time: ${getTime()}`);
+    next();
+});
+
+router.route('/news')
     .get((req, res) => res.send(news))
     .post((req, res) => {
         news.push({news: 'new', id: news.length});
         res.send(news);
     });
 
-app.route('/news/:newsId')
-    .get((req, res) => res.send(news[req.params.newsId]));
+router.route('/news/:newsId')
+    .get((req, res) => res.send(news[req.params.newsId]))
+    .delete((req, res) => res.send(news[req.params.newsId]))
 
+app.use(router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
