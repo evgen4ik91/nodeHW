8f8693b4a6cec8b127f3a8777b9e6b0febedcbef
yauheni_chyrkouski <evgen.box@live.ru>
2019-01-15 16:40:27 +0300

    put delete added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000;
 
 const news = require('./news.json');
 
+let newObj = {news: 'new', id: news.length};
+
 function getTime() {
     return new Date().toLocaleString(undefined, {
         hour: '2-digit',
@@ -17,20 +19,28 @@ function getTime() {
 }
 
 router.use(function timeLog (req, res, next) {
-    console.log( `URL: ${req.url} (${req.method}) Time: ${getTime()}`);
+    console.log( `${req.method}: ${req.url} Time: ${getTime()}`);
     next();
 });
 
 router.route('/news')
     .get((req, res) => res.send(news))
     .post((req, res) => {
-        news.push({news: 'new', id: news.length});
+        news.push();
         res.send(news);
     });
 
 router.route('/news/:newsId')
     .get((req, res) => res.send(news[req.params.newsId]))
-    .delete((req, res) => res.send(news[req.params.newsId]))
+    .put((req, res) => {
+        let id = req.params.newsId
+        news[id] = {...news[id], ...req.query};
+        res.send(news[id]);
+    })
+    .delete((req, res) => {
+        news.splice(req.params.newsId,1);
+        res.send(news);
+    })
 
 app.use(router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
