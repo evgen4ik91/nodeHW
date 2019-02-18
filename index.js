9af7cb0a3a5befc741f21bd4a9c4724bce7adf6e
evgen <evgen.box@live.ru>
2019-02-18 23:09:49 +0300

    cors fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const authFB = auth.fb;
 const app = express();
 const port = 3000;
 
-app.use(cors);
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
