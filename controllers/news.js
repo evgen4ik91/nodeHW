0f00cddbbf58a975530b49123c155cab0abad13b
yauheni_chyrkouski <evgen.box@live.ru>
2019-02-18 17:28:24 +0300

    changes

--- controllers/news.js
+++ controllers/news.js
@@ -4,35 +4,35 @@ const News = require('../schema/news');
 const checkAuth = require('../functions').checkAuth;
 
 router.route('/')
-    .get(checkAuth, (req,res) => {
+	.get(checkAuth, (req,res) => {
 		News.find({})
 			.then(docs => {
 				res.send(docs);
 			}).catch(err => console.log(err));
 	})
-    .post((req,res) => {
-			new News(req.body).save()
-				.then(() => {
-					console.log('news saved');
-					res.send(req.body);
-				}).catch(err => {
-					res.send(null);
-					console.log(err);
-				});
+	.post((req,res) => {
+		new News(req.body).save()
+			.then(() => {
+				console.log('news saved');
+				res.send(req.body);
+			}).catch(err => {
+				res.send(null);
+				console.log(err);
+			});
 	});
 	
 router.route('/:newsId')
-    .get((req, res) => {
+	.get((req, res) => {
 		News.find({ id:req.params.newsId })
 			.then(docs => res.send(docs))
 			.catch(err => console.log(err));
 	})
-    .put(checkAuth, (req,res) => {
+	.put(/*checkAuth,*/ (req,res) => {
 		News.updateOne({ id:req.params.newsId }, req.body)
 			.then(docs => res.send(docs))
 			.catch(err => console.log(err));
-    })
-    .delete(checkAuth, (req,res) => {
+	})
+	.delete(/*checkAuth,*/ (req,res) => {
 		News.deleteOne({ id:req.params.newsId })
 			.then(docs => res.send(docs))
 			.catch(err => console.log(err));
